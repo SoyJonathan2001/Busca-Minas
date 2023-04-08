02bef1f4a5b3ba8cc6d890752ee079e662c1521a
SoyJonathan2001 <jonathan.castrojc01@estudiantec.cr>
2023-04-08 04:02:44 -0600

    Juego Base
    Se carga la base del juego

---
+++ src/main/java/Game.java
@@ -0,0 +1,18 @@
+public class Game {
+
+    public static final int WIDTH = 500, HEIGHT = 500;
+    public static final int GRIDSIZE = 8;
+    public static final int MINECOUNT = (int) Math.round(GRIDSIZE * GRIDSIZE * .1);
+
+    private Handler handler = new Handler();
+
+    public Game() {
+        new Window(WIDTH, HEIGHT, GRIDSIZE, "Minesweeper - ", this, handler);
+        Window.update(0);
+    }
+
+    public static void main(String[] args) {
+        new Game();
+    }
+
+}
